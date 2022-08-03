1fcec1513bd858b1dc18e4c56dfec5d30ec9cd0c
Aryan <aryanrsuri@gmail.com>
2022-08-03 09:41:07 -0700

    1.2.0

--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
 const NUMBERS = document.querySelectorAll("[data-number]");
 const OPERATOR = document.querySelectorAll("[data-operator]");
+const RENDER = document.getElementsByClassName("output")[0];
+const EVAL = document.getElementById("evaluate");
+const CLEAR = document.getElementById("clear");
+const DEL = document.getElementById("delete");
+let VALUES = [];
 
-console.log(NUMBERS[0].innerText, OPERATOR[0].innerText);
+NUMBERS.forEach((btn) => {
+  btn.addEventListener("click", render);
+});
 
+OPERATOR.forEach((btn) => {
+  btn.addEventListener("click", renderOperator);
+});
 
+EVAL.addEventListener("click", evaluate);
+CLEAR.addEventListener("click", clear);
+DEL.addEventListener("click", del);
 function add(a, b) {
   return a + b;
 }
@@ -17,16 +30,56 @@ function divide(a, b) {
   return a / b;
 }
 
+function clear() {
+  VALUES = [];
+  RENDER.innerHTML = ``;
+}
+
+function del() {
+  RENDER.innerHTML = RENDER.innerHTML.slice(0, -1);
+}
+
 async function operate(opr, a, b) {
   const OPR = {
     "+": add,
     "-": subtract,
-    "*": multiply,
+    x: multiply,
     "/": divide,
   };
+
   try {
     const operation = OPR[opr];
-    return Promise.resolve(operation(a, b));
+    return Promise.resolve(parseFloat(operation(a, b)).toFixed(3));
+  } catch (Error) {
+    return Promise.reject(Error);
+  }
+}
+
+async function render() {
+  try {
+    VALUES.push(parseFloat(this.textContent));
+    Promise.resolve((RENDER.innerHTML += `${this.textContent}`));
+  } catch (Error) {
+    return Promise.reject(Error);
+  }
+}
+async function renderOperator() {
+  if (RENDER.innerHTML == ``) {
+    throw "Must have a value before operator!";
+  }
+  try {
+    Promise.resolve(RENDER.innerHTML += `${this.textContent}`);
+    VALUES.push(this.textContent);
+  } catch(Error) {
+    Promise.reject(Error);
+  } 
+}
+
+async function evaluate() {
+  try {
+    const result = await operate(VALUES[1], VALUES[0], VALUES[2]);
+    Promise.resolve((RENDER.innerHTML = `${result}`));
+    VALUES = [];
   } catch (Error) {
     return Promise.reject(Error);
   }
